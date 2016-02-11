22b93c8faf1ff80889b2b91a116fb6f37c43a9ae
Abirafdi Raditya Putra <abirafdiraditya@gmail.com>
2016-02-11 17:59:25 +0700

    nuke

--- utils/deprecation.py
+++
@@ -1,6 +0,0 @@
-class RemovedInWagtail11Warning(DeprecationWarning):
-    pass
-
-
-class RemovedInWagtail12Warning(PendingDeprecationWarning):
-    pass
--- wagtailforms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from wagtail.wagtailforms import views
-
-
-urlpatterns = [
-    url(r'^$', views.index, name='wagtailforms_index'),
-    url(r'^submissions/(\d+)/$', views.list_submissions, name='wagtailforms_list_submissions'),
-]
